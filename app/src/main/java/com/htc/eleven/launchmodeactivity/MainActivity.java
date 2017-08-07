35f00be5aaa97b81745678a33015541a53e19df6
eleven_xiang <eleven_xiang@htc.com>
2017-08-07 10:31:37 +0800

    Learn about Activity launch mode: standard & singleTop mode.

---
+++ app/src/main/java/com/htc/eleven/launchmodeactivity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.htc.eleven.launchmodeactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.text_view);
+
+        tv.setText(String.format("Current Task ID: %d\n, Activity info: %s ! ", getTaskId(), toString()));
+
+        findViewById(R.id.btnStartMainActivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+
+        findViewById(R.id.btnStartSubActivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SubActivity.class));
+            }
+        });
+    }
+}
